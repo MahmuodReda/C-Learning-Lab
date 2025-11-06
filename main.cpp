9bca6aa4d5abda1d5cd06187beda6a0f6cdc9673
Mahmood_Reda <mr4141705@gmail.com>
2025-11-06 23:31:31 +0300

    Demo initializer list with const string and reference member.

--- main.cpp
+++ main.cpp
@@ -2,60 +2,25 @@
 #include <string>
 using namespace std;
 
-// Default Constructor
-class Car {
+class Person {
+    const string name; // must use initializer list
+    int age;
+    int &refAge;       // reference must be initialized
 public:
-    Car() {
-        cout << "Default Constructor: Car created with default values." << endl;
-    }
-};
-
-// Parameterized Constructor
-class Engine {
-public:
-    Engine(int hp) {
-        cout << "Parameterized Constructor: Engine with " << hp << " HP created." << endl;
-    }
-    Engine (){
-          cout << "Default Constructor" << endl; 
-    }
-};
-
-// Delegating Constructor
-class Vehicle {
-    int a ; 
-    int b ;
-public:
-    // Constructor with parameters
-    Vehicle(int b ) {
-        cout << "Parameterized constructor 1  " <<  endl;
-             std::cout << "b = " << b << std::endl;
+    Person(string n, int a, int &ref) 
+        : name(n), age(a), refAge(ref) { // initializer list used here
+        cout << "Person Created: " << name << ", " << age << endl;
     }
 
-    // Delegating Constructor -> calls the parameterized one
-    Vehicle(int a ,int b )  : Vehicle( b) {
-        cout << "Parameterized constructor 2" << endl;
-        std::cout << "a = " << a << std::endl;
-         std::cout << "b = " << b << std::endl;
+    void show() {
+        cout << "Name: " << name << ", Age: " << age << ", Ref: " << refAge << endl;
     }
 };
 
 int main() {
-    Car c1;                    // Default constructor
-    Engine e1(200);        // Parameterized constructor
-    Engine e2 ;
-    Vehicle v1(5);          // Delegating constructor
-    Vehicle v2(1 , 2) ;  // Direct parameterized constructor
+    int realAge = 30;
+    Person p1("Mahmoud", 25, realAge);
+    p1.show();
+
     return 0;
 }
-
-// Default Constructor: Car created with default values.
-// Parameterized Constructor: Engine with 200 HP created.
-// Default Constructor
-// Parameterized constructor 1
-// b = 5
-// Parameterized constructor 1
-// b = 2
-// Parameterized constructor 2
-// a = 1
-// b = 2
