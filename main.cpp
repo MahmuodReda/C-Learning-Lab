30b4f5df8de2d1d7d32ff9a3b65264629fb06122
Mahmood_Reda <mr4141705@gmail.com>
2025-11-07 02:06:42 +0300

    Show constructor initializer list issue with member initialization order .

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Person {
-    const string name; // must use initializer list
-    int age;
-    int &refAge;       // reference must be initialized
+class Example {
+    int a;
+    int b;
 public:
-    Person(string n, int a, int &ref) 
-        : name(n), age(a), refAge(ref) { // initializer list used here
-        cout << "Person Created: " << name << ", " << age << endl;
-    }
-
-    void show() {
-        cout << "Name: " << name << ", Age: " << age << ", Ref: " << refAge << endl;
+    // Here we try to initialize members using initializer list
+    // But the order of initialization follows the order of declaration in the class
+    // First 'b' is initialized because it is declared before 'a'
+    // Then 'a' is initialized using 'b'
+    // This can lead to unexpected values if we use a member before it is set
+    Example(int x, int y) : b(x), a(b) { //'Example::b' will be initialized after [-Wreorder]
+        cout << "a = " << a << ", b = " << b << endl;
     }
 };
 
 int main() {
-    int realAge = 30;
-    Person p1("Mahmoud", 25, realAge);
-    p1.show();
-
-    return 0;
+    Example obj(10, 20); // passing 10 and 20, but 'a' depends on 'b'
 }
+
+// Output may look strange like: a = 32758, b = 10
+// Reason: initialization order follows declaration order, not the list order
