982ff145bb24529a31dc4f1fd0a6abb0168d7b18
Mahmood_Reda <mr4141705@gmail.com>
2025-11-26 19:11:50 +0300

    Hybrid Inheritance demonstration .

--- main.cpp
+++ main.cpp
@@ -3,48 +3,96 @@
 class A
 {
 public:
-    A() { std::cout << "A: Constructor\n"; } // Constructor
-    ~A() { std::cout << "A: Destructor\n"; } // Destructor
+    A() { std::cout << "A: Constructor\n"; }
+    ~A() { std::cout << "A: Destructor\n"; }
 
-    void ShowA() { std::cout << "A: Function\n"; }
+    void ShowA()
+    {
+        std::cout << "A: Base function\n";
+    }
 };
 
-class B
+class B : public A
 {
 public:
-    B() { std::cout << "B: Constructor\n"; } // Constructor
-    ~B() { std::cout << "B: Destructor\n"; } // Destructor
+    B() { std::cout << "B: Constructor\n"; }
+    ~B() { std::cout << "B: Destructor\n"; }
 
-    void ShowB() { std::cout << "B: Function\n"; }
+    void ShowB()
+    {
+        std::cout << "B: Function\n";
+    }
 };
 
-class C : public A, public B
+class C : public A
 {
 public:
-    C() { std::cout << "C: Constructor\n"; } // Constructor
-    ~C() { std::cout << "C: Destructor\n"; } // Destructor
+    C() { std::cout << "C: Constructor\n"; }
+    ~C() { std::cout << "C: Destructor\n"; }
 
-    void ShowC() { std::cout << "C: Function\n"; }
+    void ShowC()
+    {
+        std::cout << "C: Function\n";
+    }
+};
+
+class D : public B, public C
+{
+public:
+    D() { std::cout << "D: Constructor\n"; }
+    ~D() { std::cout << "D: Destructor\n"; }
+
+    void ShowD()
+    {
+        std::cout << "D: Final child\n";
+    }
 };
 
 int main()
 {
-    C obj;
-    obj.ShowA(); // Accessing base class A function
-    obj.ShowB(); // Accessing base class B function
-    obj.ShowC(); // Accessing derived class C function
+    {
+        D obj;
+        obj.ShowB();
+        obj.ShowC();
+        obj.ShowD();
+    }
+
+    std::cout << "*************************************" << std::endl;
+    D obj;
+    // obj.ShowA(); //Error "D::ShowA" is ambiguous
+    obj.B::ShowA(); // Resolving ambiguity by specifying the path
+
+    return 0;
 }
 
-//   A     B
-//    \   /
-//      C
-// Multiple Inheritance Example
+//     A
+//    / \
+//   B   C
+//    \ /
+//     D
+
 // A: Constructor
 // B: Constructor
+// A: Constructor
 // C: Constructor
-// A: Function
+// D: Constructor
 // B: Function
 // C: Function
+// D: Final child
+// D: Destructor
 // C: Destructor
+// A: Destructor
+// B: Destructor
+// A: Destructor
+// *************************************
+// A: Constructor
+// B: Constructor
+// A: Constructor
+// C: Constructor
+// D: Constructor
+// A: Base function
+// D: Destructor
+// C: Destructor
+// A: Destructor
 // B: Destructor
 // A: Destructor
