9edec2f9f17c08e961c4fccf6d7596599d0bac32
Mahmood_Reda <mr4141705@gmail.com>
2025-11-26 17:52:39 +0300

    Single Inheritance demonstration .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com" << std::endl;
-    return 0;
+class A
+{
+public:
+    A() { std::cout << "A: Constructor\n"; } // Constructor
+    ~A() { std::cout << "A: Destructor\n"; } // Destructor
+
+    void Show()
+    {
+        std::cout << "A: Parent function\n";
+    }
+};
+
+class B : public A
+{
+public:
+    B() { std::cout << "B: Constructor\n"; } // Constructor
+    ~B() { std::cout << "B: Destructor\n"; } // Destructor
+
+    void Print()
+    {
+        std::cout << "B: Child function\n";
+    }
+};
+
+int main()
+{
+    B obj;
+    obj.Show();  // Call parent class function
+    obj.Print(); // Call child class function
 }
+
+// Class A
+//  ↓
+// Class B
+// Inheritance: B inherits from A
+
+// A: Constructor
+// B: Constructor
+// A: Parent function
+// B: Child function
+// B: Destructor
+// A: Destructor
