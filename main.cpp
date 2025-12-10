9d3aebc8752094b3aabb03e06784f48c6ef96407
Mahmood_Reda <mr4141705@gmail.com>
2025-12-10 17:14:56 +0300

    Code shows try/catch handling division by zero .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com" << std::endl;
-    return 0;
+int main()
+{
+  try
+  {
+    int x = 10, y = 0;
+    if (y == 0)
+    {
+      throw std::runtime_error("Division by zero error");
+    }
+    int z = x / y;
+    std::cout << "Result: " << z << std::endl;
+  }
+
+  catch (const std::runtime_error &e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  return 0;
 }
+// Error: Division by zero error
