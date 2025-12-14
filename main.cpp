b630ad608c38cb49c6f8521695126dd77b24806e
Mahmood_Reda <mr4141705@gmail.com>
2025-12-14 22:42:13 +0300

    Commit shows template function defined inside the class .

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <memory>
 
-// /* Original Code Before Insights Transformation */
+// /* Original Code */
 // template<typename T>
-// class container
+// class container2
 // {
 
 //   public:
 //   T value;
-//   inline container(T val)
+//   inline container2(T val)
 //   : value(val)
 //   {
 //   }
 
+//   template<typename U>
+//   inline void add(U val)
+//   {
+//     this->value = static_cast<T>(this->value + val);
+//     std::operator<<(std::operator<<(std::cout, "type of U: "), typeid(U).name()).operator<<(std::endl);
+//   }
 //   inline void print()
 //   {
 //     (std::cout << this->value) << std::endl;
@@ -20,39 +26,41 @@
 
 // };
 
-// /* First instantiated from: insights.cpp:15 */
+// /* First instantiated from: insights.cpp:24 */
 // #ifdef INSIGHTS_USE_TEMPLATE
 // template<>
-// class container<int>
+// class container2<int>
 // {
 
 //   public:
 //   int value;
-//   inline container(int val)
+//   inline container2(int val)
 //   : value{val}
 //   {
 //   }
 
-//   inline void print()
+//   template<typename U>
+//   inline void add(U val);
+
+//   /* First instantiated from: insights.cpp:25 */
+//   #ifdef INSIGHTS_USE_TEMPLATE
+//   template<>
+//   inline void add<double>(double val)
 //   {
-//     std::cout.operator<<(this->value).operator<<(std::endl);
+//     this->value = static_cast<int>(static_cast<double>(this->value) + val);
+//     std::operator<<(std::operator<<(std::cout, "type of U: "), typeid(double).name()).operator<<(std::endl);
 //   }
+//   #endif
 
-// };
-
-// #endif
-// /* First instantiated from: insights.cpp:17 */
-// #ifdef INSIGHTS_USE_TEMPLATE
-// template<>
-// class container<double>
-// {
-
-//   public:
-//   double value;
-//   inline container(double val)
-//   : value{val}
+//   /* First instantiated from: insights.cpp:27 */
+//   #ifdef INSIGHTS_USE_TEMPLATE
+//   template<>
+//   inline void add<int>(int val)
 //   {
+//     this->value = static_cast<int>(this->value + val);
+//     std::operator<<(std::operator<<(std::cout, "type of U: "), typeid(int).name()).operator<<(std::endl);
 //   }
+//   #endif
 
 //   inline void print()
 //   {
@@ -65,28 +73,52 @@
 
 // int main()
 // {
-//   container<int> c1 = container<int>(10);
-//   c1.print();
-//   container<double> c2 = container<double>(3.1400000000000001);
-//   c2.print();
+//   container2<int> c = container2<int>(10);
+//   c.add<double>(2.5);
+//   c.print();
+//   c.add<int>(5);
+//   c.print();
 //   return 0;
 // }
+
+// template<typename T>
+// container2(T val) -> container2<T>;
+
+// /* First instantiated from: insights.cpp:24 */
+// #ifdef INSIGHTS_USE_TEMPLATE
+// template<>
+// container2(int val) -> container2<int>;
+// #endif
+
 template <typename T>
-class container
+class container2
 {
 public:
   T value;
 
-  container(T val) : value(val) {}
+  container2(T val) : value(val) {}
+
+  template <typename U>
+  void add(U val)
+  {
+    value += val;
+    std::cout << "type of U: " << typeid(U).name() << std::endl;
+  }
+
   void print() { std::cout << value << std::endl; }
 };
 
 int main()
 {
-  container<int> c1(10);
-  c1.print();
-  container<double> c2(3.14);
-  c2.print();
+  container2 c(10);
+  c.add(2.5);
+  c.print(); // Should output 12
+  c.add(5);
+  c.print(); // Should output 17
 
   return 0;
 }
+// type of U: d
+// 12
+// type of U: i
+// 17
