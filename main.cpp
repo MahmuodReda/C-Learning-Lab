447a8ed474fa87f3b69593e129f388439bb63888
Mahmood_Reda <mr4141705@gmail.com>
2025-12-16 16:43:34 +0300

    Shows threads: main and worker run, join waits for finish .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <thread>
 
-int main() {
-    std::cout << "Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com" << std::endl;
-    return 0;
+void worker()
+{
+  std::cout << " [0]Hello from worker thread\n";
+  std::cout << " [1]Hello from worker thread\n";
+  std::cout << " [2]Hello from worker thread\n";
 }
+
+int main()
+{
+  std::thread t(worker); // create new thread
+
+  std::cout << " [0]Hello from main thread\n";
+  std::cout << " [1]Hello from main thread\n";
+  std::cout << " [2]Hello from main thread\n";
+  std::cout << " [3]Hello from main thread\n";
+
+  t.join(); // wait for thread to finish  Error: terminate called without an active exception
+  std::cout << " [4]Hello from main thread\n";
+  return 0;
+}
+
+//  [0]Hello from main thread
+//  [1]Hello from main thread
+//  [0]Hello from worker thread
+//  [1]Hello from worker thread
+//  [2]Hello from worker thread
+//  [2]Hello from main thread
+//  [3]Hello from main thread
+//  [4]Hello from main thread
