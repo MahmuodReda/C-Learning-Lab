ea3a6f0b082456bd9b89a768e1c8cc91a202c61d
Mahmood_Reda <mr4141705@gmail.com>
2025-12-25 00:45:10 +0300

    Task 0: run system commands with delays (open file, echo text, launch calculator) .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <cstdlib>
+#include <thread>
 
-int main() {
-    std::cout << "Full Name: Mahmoud Reda\nBirth Date: 2001/01/15\nE-mail: mr4141705@gmail.com" << std::endl;
-    return 0;
+int main()
+{
+  system("\"D:\\Mahmood Reda\\C++Learning-Lab\\.gitignore\"");  // Attempt to open a .gitignore file
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Wait for 1 second
+  system("echo Hello, World!");                                 // Print Hello, World! to the console
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Wait for 1 second
+  system("calc.exe");                                           // Open Calculator
+
+  return 0;
 }
