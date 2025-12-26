855401eefdad5b08555a99dbba2aed906e777629
Mahmood_Reda <mr4141705@gmail.com>
2025-12-27 00:56:09 +0300

    Task 2: handle raw HTTP request, extract path, and send plain-text response .

--- server.cpp
+++ server.cpp
@@ -1,6 +1,19 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <functional>
+#include <cwchar>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <cstring>
+#include <psdk_inc/_wsadata.h>
+#include <minwindef.h>
+#include <psdk_inc/_socket_types.h>
+#include <psdk_inc/_ip_types.h>
+#include <inaddr.h>
+#include <c++/15.2.0/bits/basic_string.h>
+#include <c++/15.2.0/bits/ranges_base.h>
 
 int main()
 {
@@ -62,18 +75,41 @@ int main()
     std::cout << "Client connected!\n";
 
     // 7) recv
-    char buffer[512]{};
-    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-
+    char bufferr[2048];
+    int bytesReceived = recv(clientSocket, bufferr, sizeof(bufferr) - 1, 0);
+    std::string buffer = bufferr;
     if (bytesReceived > 0)
     {
         buffer[bytesReceived] = '\0';
-        std::cout << "Received: " << buffer << "\n";
+
+        std::cout << "----- RAW HTTP REQUEST -----\n";
+        std::cout << buffer << "\n";
+        std::cout << "----------------------------\n";
+
+        size_t start = buffer.find("GET ") + 4;
+        size_t end = buffer.find(" HTTP/1.1", start);
+        std::string path = buffer.substr(start, end - start);
+        std::cout << path << std::endl;
+
+        const char *body =
+            "Hallo from C++20 Server\r\n"
+            "(Mahmoud Reda)\r\n";
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: " +
+            std::to_string(strlen(body)) + "\r\n"
+                                           "\r\n" +
+            std::string(body);
+
+        send(clientSocket, response.c_str(), response.size(), 0);
     }
     else
     {
         std::cerr << "Receive failed\n";
     }
+    std::cerr << "Receive failed\n";
 
     // 8) cleanup
     closesocket(clientSocket);
@@ -82,3 +118,21 @@ int main()
 
     return 0;
 }
+
+// Server starting...
+// Server listening on port 5000...
+// Client connected!
+// ----- RAW HTTP REQUEST -----
+// GET /open/calc HTTP/1.1
+// Host: 192.168.1.7:5000
+// Connection: keep-alive
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Linux; Android 16; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.7499.116 Mobile Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+// X-Requested-With: org.telegram.messenger
+// Accept-Encoding: gzip, deflate
+// Accept-Language: de,de-DE;q=0.9,ar-AE;q=0.8,ar;q=0.7,en-DE;q=0.6,en-US;q=0.5,en;q=0.4
+
+// ----------------------------
+// /open/calc
+// Receive failed
