58581e3ecd23a06b995dbdf5d4aac75afab99435
Mahmood_Reda <mr4141705@gmail.com>
2025-12-31 19:47:20 +0300

    add CMake config with cache variables, versioning, and configure_file support .

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include "calc.hpp"
 #include "wifi.hpp"
+#include "defaultConfig.hpp"
 
 int main()
 {
 
+    std::cout << "Product Type: " << ProductType << std::endl;
+    std::cout << "Product Year: " << PRODUCT_YEAR << std::endl;
+    std::cout << "HelloApp Version: " << HELLOAPP_VERSION_MAJOR << "." << HELLOAPP_VERSION_MINOR << std::endl;
+
     wifi_init();
-    std::cout << X << std::endl;
-    std::cout << "Sum of 3 and 5 is: " << sum(3, 5) << std::endl;
-    int a = 5, b = 7;
-    std::cout << "sum(" << a << ", " << b << ") = " << sum(a, b) << std::endl;
+
     return 0;
 }
+
+// Product Type: BMW
+// Product Year: 2025
+// HelloApp Version: 1.0
+// WiFi initialized.
