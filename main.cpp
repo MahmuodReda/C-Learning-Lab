ae2b2050785f83c4286e419d28c87fa6f11665b1
Mahmood_Reda <mr4141705@gmail.com>
2026-01-01 18:39:53 +0300

    add CMake function test using arguments, ARGN, and ARGC .

--- main.cpp
+++ main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 #include "calc.hpp"
 #include "wifi.hpp"
-#include "defaultConfig.hpp"
 
 int main()
 {
 
-    std::cout << "Product Type: " << ProductType << std::endl;
-    std::cout << "Product Year: " << PRODUCT_YEAR << std::endl;
-    std::cout << "Debug Mode: " << DEBUGINFO << std::endl;
     wifi_init();
-
     return 0;
 }
-
-// Product Type: BMW
-// Product Year: 2025
-// HelloApp Version: 1.0
-// WiFi initialized.
