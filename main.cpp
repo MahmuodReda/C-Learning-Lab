ea710e9aa355b7c3eadce704ebe1872873020645
Mahmood_Reda <mr4141705@gmail.com>
2026-01-04 21:26:04 +0300

    configure CMake to find and link fmt + spdlog libraries .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "calc.hpp"
 #include "wifi.hpp"
+#include <fmt/core.h>
+#include <spdlog/spdlog.h>
 
 int main()
 {
 
+    // c = 10;
+    spdlog::info("Hello from spdlog via vcpkg + MSVC");
+    std::cout << "Hello, World!" << std::endl;
+    fmt::print("Hello, vcpkg fmt!\n");
     wifi_init();
+    spdlog::info("Hello from spdlog via vcpkg + MSVC");
     return 0;
 }
